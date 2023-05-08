47fadb32dc45cece8518be72d7a98b448369c1f0
Eric Lin <es3lin@uwaterloo.ca>
2023-05-08 10:42:13 -0400

    Initial Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		'./node_modules/flowbite-react/**/*.js',
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+		'./app/**/*.{js,ts,jsx,tsx}',
+		'./src/**/*.{js,jsx,ts,tsx}',
+		'./node_modules/react-tailwindcss-datepicker/dist/index.esm.js',
+	],
+	theme: {
+		colors: {
+			background: '#DCEED1',
+			'pale-blue': '#7697d6',
+			'pale-purple': '#E5D9F2',
+			gunmetal: '#253237',
+			'ash-gray': '#AAC0AA',
+			'off-white': '#f2f3f4',
+			viridian: '#417B5A',
+		},
+		fontFamily: {
+			Inter: ['Inter', 'sans-serif'],
+			Lexend: ['Lexend', 'sans-serif'],
+		},
+		extend: {
+			animation: {
+				shortpulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+			},
+		},
+	},
+	plugins: [require('flowbite/plugin')],
+};
